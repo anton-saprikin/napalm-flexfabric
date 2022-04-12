17716c446862dd919b0206f3ef690b4494fb2808
anton-saprikin <99949425+anton-saprikin@users.noreply.github.com>
2022-04-12 17:18:01 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,23 @@
 import uuid
 import setuptools
 try:
+    # pip >=20
+    from pip._internal.network.session import PipSession
     from pip._internal.req import parse_requirements
 except ImportError:
-    from pip.req import parse_requirements
+    try:
+        # 10.0.0 <= pip <= 19.3.1
+        from pip._internal.download import PipSession
+        from pip._internal.req import parse_requirements
+    except ImportError:
+        # pip <= 9.0.3
+        from pip.download import PipSession
+        from pip.req import parse_requirements
 
 __author__ = 'Steffen Walter <steffen.walter@atos.net>'
 
 install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
-reqs = [str(ir.req) for ir in install_reqs]
+reqs = [str(ir.requirement) for ir in install_reqs]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
